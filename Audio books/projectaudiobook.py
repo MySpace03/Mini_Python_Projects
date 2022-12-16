a4692919ddbd220960a3817d9a70448f309ffa15
MySpace03 <120674982+MySpace03@users.noreply.github.com>
2022-12-16 17:35:44 +0530

    Add files via upload

---
+++ Audio books/projectaudiobook.py
@@ -0,0 +1,9 @@
+import pyttsx3
+#file = open(filename, encoding="utf8")
+book=open(r"D:\VS Code\When Dimple Met Rishi (Menon Sandhya) (z-lib.org).txt",encoding="utf8")
+book_text=book.readlines()
+engine = pyttsx3.init()
+for i in book_text:
+    print(i)
+    engine.say(i)
+    engine.runAndWait()
