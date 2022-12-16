984dc377278a7937f7b18dd8d7f50841fee7f6de
MySpace03 <120674982+MySpace03@users.noreply.github.com>
2022-12-16 17:30:56 +0530

    Update projectaudiobook.py

--- projectaudiobook.py
+++ projectaudiobook.py
@@ -1,6 +1,6 @@
 import pyttsx3
 #file = open(filename, encoding="utf8")
-book=open(r"D:\VS Code\When Dimple Met Rishi (Menon Sandhya) (z-lib.org).txt",encoding="utf8")
+book=open(r"D:\VS Code\When Dimple Met Rishi (Menon Sandhya).txt",encoding="utf8")
 book_text=book.readlines()
 engine = pyttsx3.init()
 for i in book_text:
