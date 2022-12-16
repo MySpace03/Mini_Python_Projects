77169bfd8597c804e78668053083b1f1c529e627
MySpace03 <120674982+MySpace03@users.noreply.github.com>
2022-12-16 17:35:14 +0530

    Delete projectaudiobook.py

--- projectaudiobook.py
+++
@@ -1,9 +0,0 @@
-import pyttsx3
-#file = open(filename, encoding="utf8")
-book=open(r"When Dimple Met Rishi (Menon Sandhya) (z-lib.org).txt",encoding="utf8")
-book_text=book.readlines()
-engine = pyttsx3.init()
-for i in book_text:
-    print(i)
-    engine.say(i)
-    engine.runAndWait()
